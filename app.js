74e8b1d17b032551882290d7bf9c921df9d121af
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-11-17 14:30:52 +0300

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const { PORT = 3000 } = process.env;
 
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
