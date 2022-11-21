5e9f3b7ffaa2ba35aa0828bd2629958012ad3c11
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-11-21 14:26:29 +0300

    feat: add bodyParser

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./routes.js/users');
 
 const { PORT = 3000 } = process.env;
 
-const mongoose = require('mongoose');
-
 const app = express();
+app.use((req, res, next) => {
+  req.user = {
+    _id: '637b5e16bf7ab33ead099014',
+  };
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  next();
 });
+app.use(bodyParser.json());
+app.use(router);
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
