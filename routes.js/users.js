8cb058bbf9eb05c5b5ef88c2b13a601303b7b420
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-11-21 22:57:03 +0300

    feat: add updating profile and avatar

--- routes.js/users.js
+++ routes.js/users.js
@@ -1,8 +1,12 @@
 const usersRouter = require('express').Router();
-const { getAllUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getAllUsers, getUserById, createUser, updateUser, updateAvatar,
+} = require('../controllers/users');
 
 usersRouter.get('/users', getAllUsers);
 usersRouter.get('/users/:userId', getUserById);
 usersRouter.post('/users', createUser);
+usersRouter.patch('/users/me', updateUser);
+usersRouter.patch('/users/me/avatar', updateAvatar);
 
 module.exports = usersRouter;
