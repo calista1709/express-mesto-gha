c14afaac9d2657a762a02e8c41111f00de4f3b7a
Yuliya Krionerova <mar4uk09@yandex.ru>
2022-11-22 00:50:46 +0300

    feat: handling data format

--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,13 @@ const User = require('../models/user');
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send({ data: users }))
+    .then((users) => res.status(200).send(users))
     .catch(() => res.status(404).send({ message: 'Информация не найдена' }));
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(200).send(user))
     .catch(() => res.status(404).send({ message: 'Запрашиваемый пользователь не найден' }));
 };
 
@@ -16,22 +16,22 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => res.status(201).send(user))
     .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
 };
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.status(200).send({ data: user }))
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => res.status(200).send(user))
     .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.status(200).send({ data: user }))
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.status(200).send(user))
     .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
 };
